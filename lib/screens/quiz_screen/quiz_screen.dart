57836527e04f60ed04a93901279564f168532cf2
ShishirRijal <ccrrizal@gmail.com>
2022-10-20 02:08:02 +0545

    quiz screen responsive

--- lib/screens/quiz_screen/quiz_screen.dart
+++ lib/screens/quiz_screen/quiz_screen.dart
@@ -13,85 +13,99 @@ class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final isMobile = size.width < 480;
+    final double scaleFactor = isMobile ? 1.0 : 1.5;
+    final padding = isMobile ? 20.0 : 40.0;
+    final smallDevicePadding = size.height < 720 ? 120.0 : 180.0;
     return Scaffold(
-      backgroundColor: Color.fromARGB(242, 253, 253, 253),
-      body: Stack(
-        alignment: Alignment.center,
-        clipBehavior: Clip.none,
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 250,
-                width: size.width,
-                decoration: const BoxDecoration(
-                  color: ColorManager.primary,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20.0),
-                    bottomRight: Radius.circular(20.0),
+      body: SingleChildScrollView(
+        child: Stack(
+          alignment: Alignment.center,
+          clipBehavior: Clip.none,
+          children: [
+            Column(
+              children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  height: isMobile ? 250 : 350,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: ColorManager.primary,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: FittedBox(
+                    child: Text(
+                      "QUIZ",
+                      style: getBoldTextStyle(
+                        color: Colors.white38,
+                        size: 200.0,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 200, 20, 0),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      padding, smallDevicePadding, padding, padding),
                   child: Column(
                     // crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      AnserCard(isTapped: true, ans: 'true'),
-                      SizedBox(height: 15.0),
-                      AnserCard(isTapped: false, ans: 'false'),
-                      SizedBox(height: 15.0),
-                      AnserCard(isTapped: true, ans: 'false'),
-                      SizedBox(height: 15.0),
-                      AnserCard(isTapped: false, ans: 'false'),
+                    children: [
+                      const AnswerCard(isTapped: true, ans: 'true'),
+                      SizedBox(height: isMobile ? 15.0 : 20.0),
+                      const AnswerCard(isTapped: false, ans: 'false'),
+                      SizedBox(height: isMobile ? 15.0 : 20.0),
+                      const AnswerCard(isTapped: true, ans: 'false'),
+                      SizedBox(height: isMobile ? 15.0 : 20.0),
+                      const AnswerCard(isTapped: false, ans: 'false'),
                       //
                     ],
                   ),
                 ),
-              ),
-            ],
-          ),
-          Positioned(
-            top: 150,
-            child: QuestionCard(size: size),
-          ),
-          Positioned(
-            top: 100.0,
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-              ),
-              child: CircularPercentIndicator(
-                backgroundColor: Colors.white,
-                radius: 50.0,
-                // animationDuration: 500,
-                lineWidth: 7.0,
-                // animation: true,
-                percent: 0.75,
-                progressColor: ColorManager.primary,
-                animateFromLastPercent: true,
-                circularStrokeCap: CircularStrokeCap.round,
-                center: Text(
-                  '18',
-                  style: getBoldTextStyle(
-                    // size: 14,
-                    color: ColorManager.primary,
+              ],
+            ),
+            Positioned(
+              top: 150,
+              child: QuestionCard(size: size),
+            ),
+            Positioned(
+              top: 100.0,
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: CircularPercentIndicator(
+                  backgroundColor: Colors.white,
+                  radius: 50.0,
+                  // animationDuration: 500,
+                  lineWidth: 7.0,
+                  // animation: true,
+                  percent: 0.75,
+                  progressColor: ColorManager.primary,
+                  animateFromLastPercent: true,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  center: Text(
+                    '18',
+                    style: getBoldTextStyle(
+                      color: ColorManager.primary,
+                    ),
+                    textScaleFactor: scaleFactor,
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class AnserCard extends StatelessWidget {
-  const AnserCard({
+class AnswerCard extends StatelessWidget {
+  const AnswerCard({
     Key? key,
     required this.isTapped,
     required this.ans,
@@ -105,17 +119,20 @@ class AnserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isMobile = MediaQuery.of(context).size.width < 480;
+    final double scaleFactor = isMobile ? 1.0 : 1.5;
+
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
       width: double.infinity,
-      height: 60.0,
+      height: isMobile ? 60.0 : 75.0,
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border.all(
           color: !isTapped
-              ? Color.fromARGB(255, 205, 204, 204)
+              ? const Color.fromARGB(255, 205, 204, 204)
               : (checkAnswer() ? ColorManager.correct : ColorManager.error),
-          width: 2.0,
+          width: 2.0 * scaleFactor,
         ),
         borderRadius: BorderRadius.circular(12.0),
       ),
@@ -123,29 +140,38 @@ class AnserCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("Nile River",
-              style: getMediumTextStyle(color: ColorManager.darkGrey)),
-          Container(
-            height: 25,
-            width: 25,
-            decoration: BoxDecoration(
-              color: !isTapped
-                  ? Colors.transparent
-                  : (checkAnswer() ? ColorManager.correct : ColorManager.error),
-              borderRadius: BorderRadius.circular(15.0),
-              border: isTapped
+          Flexible(
+            child: Text("Nile River",
+                textScaleFactor: scaleFactor,
+                style: getMediumTextStyle(color: ColorManager.darkGrey)),
+          ),
+          Flexible(
+            child: Container(
+              height: isMobile ? 25 : 35,
+              width: isMobile ? 25 : 35,
+              decoration: BoxDecoration(
+                color: !isTapped
+                    ? Colors.transparent
+                    : (checkAnswer()
+                        ? ColorManager.correct
+                        : ColorManager.error),
+                borderRadius: BorderRadius.circular(15.0),
+                border: isTapped
+                    ? null
+                    : Border.all(
+                        color: const Color.fromARGB(255, 205, 204, 204),
+                        width: 2.0 * scaleFactor,
+                      ),
+              ),
+              child: !isTapped
                   ? null
-                  : Border.all(
-                      color: const Color.fromARGB(255, 205, 204, 204),
-                      width: 2.0,
+                  : FittedBox(
+                      child: Icon(
+                        checkAnswer() ? Icons.check : Icons.close,
+                        color: Colors.white,
+                      ),
                     ),
             ),
-            child: !isTapped
-                ? null
-                : Icon(
-                    checkAnswer() ? Icons.check : Icons.close,
-                    color: Colors.white,
-                  ),
           ),
         ],
       ),
@@ -163,11 +189,21 @@ class QuestionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isMobile = size.width < 480;
+    final double scaleFactor = isMobile ? 1.0 : 1.5;
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      height: 200.0,
+      height: isMobile ? 200 : 300,
       width: size.width * 0.9,
       decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
         color: Colors.white,
         borderRadius: BorderRadius.circular(20.0),
       ),
@@ -177,15 +213,16 @@ class QuestionCard extends StatelessWidget {
           const SizedBox(height: 20.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              LabelledChip(isWrong: false, score: 06),
-              LabelledChip(isWrong: true, score: 08),
+            children: [
+              LabelledChip(isWrong: false, score: 06, scaleFactor: scaleFactor),
+              LabelledChip(isWrong: true, score: 08, scaleFactor: scaleFactor),
             ],
           ),
           const SizedBox(height: 5.0),
           Flexible(
             child: RichText(
                 textAlign: TextAlign.center,
+                textScaleFactor: scaleFactor,
                 text: TextSpan(children: [
                   TextSpan(
                       text: "Question ",
@@ -202,6 +239,7 @@ class QuestionCard extends StatelessWidget {
           Align(
             child: Text("Which is the longest river in the world?",
                 textAlign: TextAlign.center,
+                textScaleFactor: scaleFactor,
                 style: getMediumTextStyle(
                   fontWeight: FontWeight.bold,
                   color: ColorManager.darkGrey,
@@ -218,29 +256,43 @@ class LabelledChip extends StatelessWidget {
     Key? key,
     required this.isWrong,
     required this.score,
+    required this.scaleFactor,
   }) : super(key: key);
   final bool isWrong;
   final int score;
+  final double scaleFactor;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         if (!isWrong)
-          Text(score < 10 ? '0$score' : '$score',
-              style: getMediumTextStyle(color: Colors.green)),
-        Container(
-          height: 15.0,
-          width: 40.0,
-          margin: const EdgeInsets.symmetric(horizontal: 10.0),
-          decoration: BoxDecoration(
-            color: isWrong ? Colors.red : Colors.green,
-            borderRadius: BorderRadius.circular(20.0),
+          FittedBox(
+            child: Text(
+              score < 10 ? '0$score' : '$score',
+              style: getMediumTextStyle(color: Colors.green),
+              textScaleFactor: scaleFactor,
+            ),
+          ),
+        FittedBox(
+          child: Container(
+            height: 15.0 * scaleFactor,
+            width: 40.0 * scaleFactor,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+              color: isWrong ? Colors.red : Colors.green,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
           ),
         ),
         if (isWrong)
-          Text(score < 10 ? '0$score' : '$score',
-              style: getMediumTextStyle(color: Colors.red)),
+          Text(
+            score < 10 ? '0$score' : '$score',
+            style: getMediumTextStyle(
+              color: Colors.red,
+            ),
+            textScaleFactor: scaleFactor,
+          ),
       ],
     );
   }
